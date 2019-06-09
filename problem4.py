7e080fd2257ff7b614109b3e1aae6fd9c082432c
Devesh Shakya <deveshshakya773@gmail.com>
2019-06-09 21:19:33 +0530

    solution

---
+++ problem4.py
@@ -0,0 +1,12 @@
+# this program should be run from root.
+
+import os
+
+username = input("Enter username: ")
+
+if username.isalpha():
+    password = 'hello' + username
+    os.system('sudo useradd -p $(openssl passwd -1 {}) {}'.format(password, username))
+    print('User created.')
+else:
+    print('Username must contain only alphabets.')
