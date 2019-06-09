4592ab3f2e6ddced2cff9fadf4ebc38776b4594f
Devesh Shakya <deveshshakya773@gmail.com>
2019-06-10 00:58:33 +0530

    solution

---
+++ problem5.py
@@ -0,0 +1,16 @@
+from datetime import datetime as d
+
+user = input("Enter name: ")
+
+t = d.now()
+h = t.hour
+
+
+if h < 12:
+    print('Good Morning ' + user + '.')
+elif 12 < h < 17:
+    print('Good Afternoon ' + user + '.')
+elif 17 < h < 21:
+    print('Good Evening ' + user + '.')
+else:
+    print('Good Night ' + user + '.')
