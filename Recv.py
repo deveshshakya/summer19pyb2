be3c162a6e1ac0368c3c811fa0fdf28efe99b61c
Devesh Shakya <deveshshakya773@gmail.com>
2019-06-14 23:56:35 +0530

    solution updated

--- Recv.py
+++ Recv.py
@@ -1,6 +1,6 @@
 import socket
 
-recv_ip = "127.0.0.1"
+recv_ip = "172.17.0.2"
 recv_port = 4444
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,7 +8,13 @@ s.bind((recv_ip, recv_port))
 
 while True:
     data = s.recvfrom(255)
-    print("Message: " + str(data[0]) + "\nIP: " + str(data[1]))
-    s.sendto("Got it.", data[1])
-
+    print("Message: " + str(data[0].decode()) + "\nIP: " + str(data[1].decode()))
+    reply = input('Reply: ')
+    if len(reply) > 150:
+        print("Length must not exceed 150 character.")
+        continue
+    if reply == 'exit' or reply == 'Exit':
+        s.close()
+        exit(0)
+    s.sendto(reply.encode(), data[1])
 s.close()
--- Sender.py
+++ Sender.py
@@ -1,14 +1,22 @@
 import socket
 
-recv_ip = "127.0.0.1"
+recv_ip = "172.17.0.2"
 recv_port = 4444
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    msg = raw_input()
-    s.sendto(msg, (recv_ip, recv_port))
+    message = input('Message: ')
+    if len(message) > 150:
+        print("Length must not exceed 150 character.")
+        continue
+    if message == 'exit' or message == 'Exit':
+        s.close()
+        exit(0)
+    s.sendto(message.encode(), (recv_ip, recv_port))
     reply = s.recvfrom(255)
-    print("Reply from Recv: " + str(reply[0]))
-
+    if reply == 'exit' or reply == 'Exit':
+        s.close()
+        exit(0)
+    print("Reply from Recv: " + str(reply[0].decode()))
 s.close()
